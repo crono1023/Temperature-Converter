2a94690e5ef8da2c3bad89ca18d7a28e59dff1d6
crono <charlesmhainesii@gmail.com>
2020-06-12 16:35:22 -0400

    initial commit
    main function prints command line arguments

--- src/com/tourian86/Main.java
+++ src/com/tourian86/Main.java
@@ -3,10 +3,34 @@ package com.tourian86;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        for(int i = 0; i < args.length; i++){
-            System.out.println("Arg " + i + ": " + args[i]);
+	    // write your code here
+        double result = 0;
+        String resultUnit = null;
+        if(args.length < 2) {
+            System.out.println("Error not enough arguments.");
+            return;
         }
+
+        switch(args[0]){
+            case "-c":
+                result = fahrenheitToCelsius(Double.parseDouble(args[1]));
+                resultUnit = "celsius";
+                break;
+
+            case "-f":
+            case "-k":
+                break;
+
+            default:
+                throw new IllegalArgumentException();
+        }
+        if(resultUnit != null){
+            System.out.printf("Result: %.2f %s", result, resultUnit);
+        }
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit){
+        return (fahrenheit - 32) * 5/9;
     }
 
 

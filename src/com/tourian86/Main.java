656d3f7bef6a7b54708f8081704a4142ab59806d
crono <charlesmhainesii@gmail.com>
2020-06-12 18:32:31 -0400

    features
    added kelvin conversion options

--- src/com/tourian86/Main.java
+++ src/com/tourian86/Main.java
@@ -2,35 +2,71 @@ package com.tourian86;
 
 public class Main {
 
+    public static void printHelp() {
+        System.out.println("Usage temp_conv -conversion-type <temperature>");
+        System.out.println("Example (32 fahrenheit to celsius: ");
+        System.out.println("tempconv -fc 32");
+        System.out.println("32 celsius to kelvin:");
+        System.out.println("temp_conv -ck 32\n");
+        System.out.println("Converstion Type Options:");
+        System.out.println("fc, cf, ck, kc, fk, kf");
+    }
+
     public static void main(String[] args) {
 	    // write your code here
-        double result = 0;
-        String resultUnit = null;
+        double result;
+        String resultUnit;
+        String origUnit;
+        String conversionType;
+        double origTemp;
         if(args.length < 2) {
             System.out.println("Error not enough arguments.");
             return;
         }
+        conversionType = args[0];
+        origTemp = Double.parseDouble(args[1]);
 
-        switch(args[0]){
-            case "-c":
-                result = fahrenheitToCelsius(Double.parseDouble(args[1]));
+        switch(conversionType){
+            case "-fc":
+                result = fahrenheitToCelsius(origTemp);
                 resultUnit = "celsius";
+                origUnit = "fahrenheit";
                 break;
 
-            case "-f":
-                result = celsiusToFahrenheit(Double.parseDouble(args[1]));
+            case "-cf":
+                result = celsiusToFahrenheit(origTemp);
                 resultUnit = "fahrenheit";
+                origUnit = "celsius";
                 break;
 
-            case "-k":
+            case "-ck":
+                result = celsiusToKelvin(origTemp);
+                resultUnit = "kelvin";
+                origUnit = "celsius";
+                break;
+
+            case "-kc":
+                result = kelvinToCelsius(origTemp);
+                resultUnit = "celsius";
+                origUnit = "kelvin";
+                break;
+
+            case "-fk":
+                result = fahrenheitToKelvin(origTemp);
+                origUnit = "fahrenheit";
+                resultUnit = "kelvin";
+                break;
+
+            case "-kf":
+                result = kelvinToFahrenheit(origTemp);
+                origUnit = "kelvin";
+                resultUnit = "fahrenheit";
                 break;
 
             default:
                 throw new IllegalArgumentException();
         }
-        if(resultUnit != null){
-            System.out.printf("Result: %.2f %s", result, resultUnit);
-        }
+        System.out.printf("%.2f %s = %.2f %s", origTemp, origUnit, result, resultUnit);
     }
 
     public static double fahrenheitToCelsius(double fahrenheit){
@@ -40,6 +76,14 @@ public class Main {
     public static double celsiusToFahrenheit(double celsius){
         return (celsius * 9 / 5) + 32;
     }
+
+    public static double celsiusToKelvin(double celsius) { return (celsius + 273.15); }
+
+    public static double kelvinToCelsius(double kelvin) { return (kelvin + 273.15); }
+
+    public static double fahrenheitToKelvin(double fahrenheit){ return (celsiusToKelvin(fahrenheitToCelsius(fahrenheit))); }
+
+    public static double kelvinToFahrenheit(double kelvin) { return celsiusToFahrenheit(kelvinToCelsius(kelvin)); }
 
 
 }

d5cdbc4b95e0cfc4d1107aa2405f501741c79868
crono <charlesmhainesii@gmail.com>
2020-06-12 16:07:10 -0400

    initial commit
    main function prints command line arguments

---
+++ src/com/tourian86/Main.java
@@ -0,0 +1,13 @@
+package com.tourian86;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for(int i = 0; i < args.length; i++){
+            System.out.println("Arg " + i + ": " + args[i]);
+        }
+    }
+
+
+}
